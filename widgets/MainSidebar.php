1d3ec955a8c3c1562d677016174d0275a2f8e88f
Agiel Kurniawan Saputra <13nightevil@gmail.com>
2015-09-12 07:02:38 +0700

    update active items

--- widgets/MainSidebar.php
+++ widgets/MainSidebar.php
@@ -158,4 +158,31 @@ class MainSidebar extends Menu
             ]);
         }
     }
+
+    protected function isItemActive($item)
+    {
+        if (isset($item['url']) && is_array($item['url']) && isset($item['url'][0])) {
+            $route = Yii::getAlias($item['url'][0]);
+            if ($route[0] !== '/' && Yii::$app->controller) {
+                $route = Yii::$app->controller->module->getUniqueId() . '/' . $route;
+            }
+            if (trim($route, '/') !== $this->route) {
+                return false;
+            }
+            unset($item['url']['#']);
+            if (count($item['url']) > 1) {
+                $params = $item['url'];
+                unset($params[0]);
+                foreach ($params as $name => $value) {
+                    if ($value !== null && (!isset($this->params[$name]) || $this->params[$name] != $value)) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    }
 }
